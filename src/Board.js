cc4acdf8a67b521c7c4c851811e6902a79a08473
Ben Grinberg <bengrin25@gmail.com>
2015-12-02 17:46:26 -0500

    Resolved row and column conflicts

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,22 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var row = this.rows()[rowIndex];
+      var result = row.reduce(function(x, y) {
+        return x + y;
+      });
+      return result > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+      for (var i = 0; i < rows.length; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -94,12 +104,34 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var rows = this.rows();
+      var conflict = false;
+
+      for (var i = 0; i < rows.length; i++) {
+        if (rows[i][colIndex]) {
+          if (conflict) {
+            return true;
+          }
+          conflict = true;
+        }
+      }
+
+      return false;
+
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var rows = this.rows();
+
+      for (var i = 0; i < rows.length; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      }
+
+      return false;
+
     },
 
 
