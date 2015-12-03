c16294a280b829e11221e9045efe558f4307633f
Ben Grinberg <bengrin25@gmail.com>
2015-12-03 15:40:38 -0500

    Refactored

--- src/solvers.js
+++ src/solvers.js
@@ -14,49 +14,27 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = new Board({n: n}); //fixme
+  var solution;
+  var board = new Board({n: n});
 
-  for (var i = 0; i < n; i++) {
-    solution.togglePiece(i, i);
-  }
+  solution = findS(board, 0, {}, n);
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution.rows();
+  return solution;
 };
-
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  // if (n === 0 || n === 1) {
-  //   return 1;
-  // }
-  // return n * countNRooksSolutions(n - 1);
-
   var solutionCount = 0;
   var board = new Board({n: n});
-  var solutions = function(board, row) {
 
-    if (row === n) {
-      return solutionCount++;
-    }
-
-    for (var i = 0; i < n; i++) {
-      board.togglePiece(row, i);
-      if (!board.hasAnyRooksConflicts()) {
-        solutions(board, row + 1);
-      }
-      board.togglePiece(row, i);
-    }
-  }
-
-  solutions(board, 0);
+  solutionCount = count(board, 0, {}, n);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 
 };
-
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
@@ -68,25 +46,7 @@ window.findNQueensSolution = function(n) {
     return board.rows();
   }
 
-  var solutions = function(board, row) {
-
-    if (row === n) {
-      return solution = board.rows();
-    }
-
-    for (var i = 0; i < n; i++) {
-      board.togglePiece(row, i);
-      if (!board.hasAnyQueenConflictsOn(row, i)) {
-        solutions(board, row + 1);
-      }
-      if (solution) {
-        return;
-      }
-      board.togglePiece(row, i);
-    }
-  };
-
-  solutions(board, 0);
+  solution = findS(board, 0, {}, n, true);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -98,23 +58,65 @@ window.countNQueensSolutions = function(n) {
   var solutionCount = 0;
   var board = new Board({n: n});
 
-  var solutions = function(board, row) {
-
-    if (row === n) {
-      return solutionCount++;
-    }
-
-    for (var i = 0; i < n; i++) {
-      board.togglePiece(row, i);
-      if (!board.hasAnyQueenConflictsOn(row, i)) {
-        solutions(board, row + 1);
-      }
-      board.togglePiece(row, i);
-    }
-  };
-
-  solutions(board, 0);
+  solutionCount = count(board, 0, {}, n, true);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+
+window.count = function(board, row, col, n, isQueen) {
+    var solutionCount = 0;
+
+    if (row === n) {
+      return 1;
+    }
+
+    for (var i = 0; i < n; i++) {
+      if (!col[i]) {
+        board.togglePiece(row, i);
+        col[i] = true;
+        if (isQueen) {
+          if (!board.hasAnyQueenConflictsOn(row, i)) {
+            solutionCount += count(board, row + 1, col, n, isQueen);
+          }
+        } else {
+          solutionCount += count(board, row + 1, col, n);
+        }
+        board.togglePiece(row, i);
+        col[i] = false;
+      }
+    }
+    return solutionCount;
+};
+
+
+window.findS = function(board, row, col, n, isQueen) {
+  var solution;
+
+  if (row === n) {
+     solution = board.rows();
+     return solution;
+  }
+
+  for (var i = 0; i < n; i++) {
+    if (!col[i]) {
+      board.togglePiece(row, i);
+      col[i] = true;
+      if (isQueen) {
+        if (!board.hasAnyQueenConflictsOn(row, i)) {
+          solution = findS(board, row + 1, col, n, isQueen);
+        }
+      } else {
+        solution = findS(board, row + 1, col, n);
+      }
+      if (Array.isArray(solution)) {
+         return solution;
+      }
+      board.togglePiece(row, i);
+      col[i] = false;
+    }
+  }
+
+  return solution;
 };
